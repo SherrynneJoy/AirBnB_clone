689182eb2ffda728d1b2505a8be92f9c3c0982cf
JuneMbuvi <mbuvimbatha@gmail.com>
2023-10-12 02:50:51 -0700

    base model

---
+++ console.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""defines a class that creates a commandline interpreter"""
+import cmd
+
+
+class interpreter(cmd.Cmd):
+    """creates a subclass of Cmd class & creates an interpreter"""
+    intro = "june"
+    prompt = '(hbnb) '
+    
+    """other attributes"""
+    doc_header = 'Documented commands (type help <topic>): '
+    misc_header = 'misc_header'
+    undoc_header = 'undoc_header'
+
+    """to separate/divide the content, the ruler is used"""
+    ruler = '='
+
+    """method to ensure the prompt works"""
+    def do_prompt(self, line):
+        self.prompt = line
+
+    """if an empty line is inserted, ignore"""
+    def emptyline(self):
+        pass
+
+    """implement the command quit to leave the console/interpreter"""
+    def do_quit(self, arg):
+        """returns true while successful"""
+        return True
+
+    """to terminate the interpreter, the EOF is invoked"""
+    def do_EOF(self, line):
+        """returns true on success"""
+        return True
+
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) > 1:
+        interpreter().onecmd()(' '.join(sys.argv[1:]))
+    else:
+        interpreter().cmdloop()
---
+++ models/base_model.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""defines a superclass BaseModel that defines all common attributes
+/methods for other classes"""
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    """defines a superclass BaseModel & its attributes"""
+    def __init__(self, *args, **kwargs):
+        """instantiate with public attributes id and datetime"""
+        self.id = str(uuid4())
+        timefmt = "%Y-%m-%dT%H:%M:%S.%f"
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    """converting datetime strings into an object"""
+                    self.__dict__[k] = datetime.strptime(v, timefmt)
+                else:
+                    self.__dict__[k] = v
+        else:
+            models.storage.new(self)
+
+    """using the str method to return the unofficial representation
+    of object instances"""
+    def __str__(self):
+        """returns the string representation of the class name, id &
+        the dictionary representation"""
+        string += "[" + str(self.__class.__name__) + "] "
+        string += "(" + str(self.id) + ") "
+        string += str(self.__dict__)
+        return (string)
+
+    """define public instance save that updates the public instance
+    attribute updated_at with the current datetime"""
+    def save(self):
+        self.updated_at = datetime.today()
+        models.storage.save()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of
+        __dict__ of the instance"""
+        mydict = self.__dict__.copy()
+        mydict["created_at"] = self.created_at.isoformat()
+        mydict["updated_at"] = self.updated_at.isoformat()
+        mydict["__class__"] = self.__class__.__name__
+        return (mydict)
